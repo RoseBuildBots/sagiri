cef8eaaedfc2235dd97486606e3bcb57afaf74f0
RoseBuildBots <41162255+RoseBuildBots@users.noreply.github.com>
2018-07-14 00:30:21 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,8 @@
+token = 'NDY2ODc4MzY3MTg3MDc1MDcz.DiqkoA.JVgJqYhnL6yyCnKeAnCHx5OvR3E' #Put Your bots token here
+
+prefix = '*' #put prefix here
+
+link = 'https://discordapp.com/api/oauth2/authorize?client_id=466878367187075073&permissions=0&scope=bot' #put bot invite link here
+
+ownerid = '329526048553172992' #put your id here
+
